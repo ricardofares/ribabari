2a68ed8c75e5324165cfa57f6c345f4f278a5d0c
rricardof <ricardoxdd28@outlook.com>
2022-06-27 20:54:22 -0300

    It has been added the `list_node_t` and `list_t` structures.
     * `list_node_create` function has been created.
        - This function provides a way to create nodes with
          a specified content.
    
     * `list_init` function has been created.
       - This function provides a way to initialize the
         specified list.
    
     * `list_add` function has been created.
       - This function provides a way to add a node at
         the end of the list with a specified content
    
     * `list_remove_head` function has been created.
       - This function provides a way to remove the node
         at the head of the list.
    
     * `list_free` function has been created.
       - This function provides a way to release the
         memory allocated to the list.

---
+++ utils/list.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "list.h"
+
+/* List Node Functions */
+
+list_node_t *list_node_create(void *content) {
+    list_node_t *node = (list_node_t *)malloc(sizeof(list_node_t));
+
+    /* It checks if the node has not been allocated */
+    if (!node) {
+        printf("Not enough memory to allocate a node!\n");
+        return NULL;
+    }
+
+    node->content = content;
+    node->next = NULL;
+    node->prev = NULL;
+
+    return node;
+}
+
+/* List Functions */
+
+void list_init(list_t **list) {
+    (*list) = (list_t *)malloc(sizeof(list));
+
+    /* It checks if the list has not been allocated */
+    if (!(*list)) {
+        printf("Not enough memory to allocate a list!\n");
+        return;
+    }
+
+    (*list)->head = NULL;
+    (*list)->tail = NULL;
+}
+
+void list_add(list_t *list, void *content) {
+    list_node_t *node = list_node_create(content);
+
+    /* It checks if the node has not been created */
+    if (!node)
+        return;
+
+    /* It checks if the list is empty */
+    if (!list->tail)
+        list->head = node;
+    else {
+        list->tail->next = node;
+        node->prev = list->tail;
+    }
+
+    list->tail = node;
+}
+
+list_node_t *list_remove_head(list_t *list) {
+    list_node_t *temp = list->head;
+
+    if (list->head) {
+        if (list->head->next)
+            list->head->next->prev = NULL;
+        else list->tail = NULL;
+        list->head = list->head->next;
+    }
+
+    /* Detach the node from the list */
+    temp->next = NULL;
+    temp->prev = NULL;
+    return temp;
+}
+
+void list_free(list_t *list) {
+    if (list) {
+        list_node_t *curr = list->head;
+        list_node_t *temp;
+
+        while (curr) {
+            temp = curr->next;
+            free(curr);
+            curr = temp;
+        }
+
+        free(list);
+    }
+}
---
+++ utils/list.h
@@ -0,0 +1,94 @@
+#ifndef OS_PROJECT_LIST_H
+#define OS_PROJECT_LIST_H
+
+/**
+ * A doubly-linked node.
+ */
+typedef struct ListNode {
+    /**
+     * It holds the node content.
+     */
+    void *content;
+
+    /**
+     * It holds a pointer to the
+     * next linked node.
+     */
+    struct ListNode *next;
+
+    /**
+     * It holds a pointer to the
+     * previous linked node.
+     */
+    struct ListNode *prev;
+} list_node_t;
+
+/**
+ * A doubly-linked list.
+ */
+typedef struct List {
+    /**
+     * It holds a pointer to the
+     * head node.
+     */
+    list_node_t *head;
+
+    /**
+     * It holds a pointer to the
+     * tail node.
+     */
+    list_node_t *tail;
+} list_t;
+
+/* List Node Prototypes */
+
+/**
+ * It creates a node holding the
+ * specified content. Further, it
+ * returns null if the system does
+ * not have enough memory to allocate
+ * the node.
+ *
+ * @param content the content
+ *
+ * @return the created node or
+ *         NULL (not enough memory)
+ */
+list_node_t *list_node_create(void *content);
+
+/* List Prototypes */
+
+/**
+ * It initializes the list.
+ *
+ * @param list the list
+ */
+void list_init(list_t **list);
+
+/**
+ * It adds the content at the end of
+ * the linked list.
+ *
+ * @param list the list
+ * @param content the content to be
+ *                added at the end
+ */
+void list_add(list_t *list, void *content);
+
+/**
+ * It removes the list head and
+ * returns the node there located.
+ *
+ * @param list the list
+ */
+list_node_t *list_remove_head(list_t *list);
+
+/**
+ * It releases the memory allocated
+ * to the list.
+ *
+ * @param list the list to be freed
+ */
+void list_free(list_t *list);
+
+#endif // OS_PROJECT_LIST_H
