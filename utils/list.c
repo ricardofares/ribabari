85d20f77458a7626a110b69ff76f1ff107e9238b
ricardofares <ricardoxdd28@outlook.com>
2022-06-28 12:17:24 -0300

    List documentation has been propagated to its function definitions.

--- utils/list.c
+++ utils/list.c
@@ -5,6 +5,18 @@
 
 /* List Node Functions */
 
+/**
+ * It creates a node holding the
+ * specified content. Further, it
+ * returns null if the system does
+ * not have enough memory to allocate
+ * the node.
+ *
+ * @param content the content
+ *
+ * @return the created node or
+ *         NULL (not enough memory)
+ */
 list_node_t *list_node_create(void *content) {
     list_node_t *node = (list_node_t *)malloc(sizeof(list_node_t));
 
@@ -23,6 +35,11 @@ list_node_t *list_node_create(void *content) {
 
 /* List Functions */
 
+/**
+ * It initializes the list.
+ *
+ * @param list the list
+ */
 void list_init(list_t **list) {
     (*list) = (list_t *)malloc(sizeof(list_t));
 
@@ -36,6 +53,14 @@ void list_init(list_t **list) {
     (*list)->tail = NULL;
 }
 
+/**
+ * It adds the content at the end of
+ * the linked list.
+ *
+ * @param list the list
+ * @param content the content to be
+ *                added at the end
+ */
 void list_add(list_t *list, void *content) {
     list_node_t *node = list_node_create(content);
 
@@ -54,6 +79,12 @@ void list_add(list_t *list, void *content) {
     list->tail = node;
 }
 
+/**
+ * It removes the list head and
+ * returns the node there located.
+ *
+ * @param list the list
+ */
 list_node_t *list_remove_head(list_t *list) {
     list_node_t *temp = list->head;
 
@@ -71,6 +102,14 @@ list_node_t *list_remove_head(list_t *list) {
     return temp;
 }
 
+/**
+ * It removes the list tail and
+ * returns the node there located.
+ *
+ * @param list the list
+ *
+ * @return the removed tail node
+ */
 list_node_t *list_remove_tail(list_t *list) {
     list_node_t *temp = list->tail;
 
@@ -88,6 +127,12 @@ list_node_t *list_remove_tail(list_t *list) {
     return temp;
 }
 
+/**
+ * It releases the memory allocated
+ * to the list.
+ *
+ * @param list the list to be freed
+ */
 void list_free(list_t *list) {
     if (list) {
         list_node_t *curr = list->head;
