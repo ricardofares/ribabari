0f42aeffaedaa819a7f4ff527ee3d976cdf5d582
ricardofares <ricardoxdd28@outlook.com>
2022-07-07 13:32:05 -0300

    Added `read_semaphores` function and changed `process_created` function name to `process_create`.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -6,7 +6,7 @@
 
 /* Internal Function Prototypes */
 
-void process_created(const char* filename);
+void process_create(const char* filename);
 
 /* Kernel Function Definitions */
 
@@ -39,9 +39,47 @@ void kernel_init() {
 }
 
 void sysCall(kernel_function_t func, void *arg) {
+    switch (func) {
+        case PROCESS_CREATE: {
+            process_create((char *)arg);
+            break;
+        }
+    }
 }
 
 /* Internal Function Definitions */
+
+void read_semaphores(process_t* proc, char* sem_line) {
+    const size_t len = strlen(sem_line);
+    int sem_count = 1;
+    int i;
+
+    /* Count the amount of semaphores requested */
+    for (i = 0; i < len; i++)
+        if (sem_line[i] == ' ') sem_count++;
+
+    proc->semaphores = (char **)malloc(sizeof(char *) * sem_count);
+
+    /* It checks if the array of semaphores could not be allocated */
+    if (!proc->semaphores) {
+        printf("Semaphores could not be allocated.\n");
+        exit(0);
+    }
+
+    char* tok = strtok(sem_line, " ");
+
+    i = 0;
+    do {
+        proc->semaphores[i] = strdup(tok);
+        i++;
+    } while ((tok = strtok(NULL, " ")));
+#if DEBUG
+    printf("Process %s requested %d semaphores.\n", proc->name, sem_count);
+    printf("Semaphores are: \n");
+    for (i = 0; i < sem_count; i++)
+        printf(" - %s\n", proc->semaphores[i]);
+#endif
+}
 
 /**
  * It parses the synthetic program written in a file.
@@ -93,5 +131,14 @@ process_t* parse_synthetic_program(const char* filepath) {
     fgets(buf, buflen, fp);
     proc->seg_size = atoi(buf);
 
+    fgets(buf, buflen, fp);
+    /* It checks if there are semaphores to be read */
+    if (strcmp(buf, "\n") != 0)
+        read_semaphores(proc, buf);
+
     return proc;
+}
+
+void process_create(const char* filename) {
+    parse_synthetic_program(filename);
 }
