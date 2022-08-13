e268a65e1eede4ffe05da29c4f60d9638128859c
rricardof <ricardoxdd28@outlook.com>
2022-08-13 08:57:06 -0300

    Comments have been added to the constants.

--- constants.h
+++ constants.h
@@ -7,19 +7,47 @@
 
 /* General Debug Options */
 
+/**
+ * It enables all subsystems from the
+ * operating system simulator debugging
+ * system.
+ */
 #define OS_DEBUG 0
 
 /* Kernel Debug Options */
 
+/**
+ * It enables the kernel debugging system,
+ * as well as cited before, if OS_DEBUG is
+ * activated then this debugging system is
+ * activated as well.
+ */
 #define OS_KERNEL_DEBUG 0
+
+/**
+ * It enables the evaluation debug part
+ * from the kernel debugging system.
+ */
 #define OS_KERNEL_EVAL_DEBUG 0
 
 /* Process Debug Options */
 
+/**
+ * It enables the process debugging system,
+ * as well as cited before, if OS_DEBUG is
+ * activated then this debugging system is
+ * activated as well.
+ */
 #define OS_PROCESS_DEBUG 0
 
 /* File System Debug Options */
 
+/**
+ * It enables the file system debugging system,
+ * as well as cited before, if OS_DEBUG is
+ * activated then this debugging system is
+ * activated as well.
+ */
 #define OS_FS_DEBUG 0
 
 #endif // OS_PROJECT_CONSTANTS_H
